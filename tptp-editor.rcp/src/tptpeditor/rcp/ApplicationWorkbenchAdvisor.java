b163ca6da5e304219c4847852764c33d131ea1a9
Olivier ROLAND <olivier.roland@edla.org>
2012-09-08 18:35:55 +0200

    Show splash screen

--- tptp-editor.rcp/src/tptpeditor/rcp/ApplicationWorkbenchAdvisor.java
+++ tptp-editor.rcp/src/tptpeditor/rcp/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,6 @@
 package tptpeditor.rcp;
 
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -10,7 +11,16 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
 	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
 			IWorkbenchWindowConfigurer configurer) {
-		return new ApplicationWorkbenchWindowAdvisor(configurer);
+		return new ApplicationWorkbenchWindowAdvisor(configurer) {
+			public void preWindowOpen() {
+				super.preWindowOpen();
+				IWorkbenchWindowConfigurer wc = getWindowConfigurer();
+				wc.setInitialSize(new Point(600, 450));
+				wc.setShowCoolBar(true);
+				wc.setShowStatusLine(true);
+				wc.setShowProgressIndicator(true);
+			}
+		};
 	}
 
 	public String getInitialWindowPerspectiveId() {
